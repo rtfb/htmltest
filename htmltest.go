6034f283edd1c3ec887037102aa2c1bb820b6e90
Vytautas Šaltenis <vytas@rtfb.lt>
2015-12-28 19:47:55 +0200

    Bring over test client and server handling funcs

--- htmltest.go
+++ htmltest.go
@@ -1,12 +1,44 @@
-package main
+package htmltest
 
 import (
 	"io/ioutil"
+	"net/http"
+	"net/http/cookiejar"
+	"net/http/httptest"
+	"net/url"
 	"testing"
 
 	"github.com/rtfb/go-html-transform/css/selector"
 	"golang.org/x/net/html"
 )
+
+var (
+	tclient *http.Client
+	tserver *httptest.Server
+)
+
+func initClient() {
+	jar, err := cookiejar.New(nil)
+	if err != nil {
+		panic(err)
+	}
+	tclient = &http.Client{
+		Jar: jar,
+	}
+}
+
+func initServer(router http.Handler) {
+	tserver = httptest.NewServer(router)
+}
+
+func Init(router http.Handler) {
+	initClient()
+	initServer(router)
+}
+
+func Client() *http.Client {
+	return tclient
+}
 
 func CssSelect(t *testing.T, node *html.Node, query string) []*html.Node {
 	chain, err := selector.Selector(query)
@@ -36,4 +68,32 @@ func Curl(url string) string {
 
 func CurlPost(url string) string {
 	return curlParam(url, tclientPostForm)
+}
+
+func PostForm(path string, values *url.Values) (string, error) {
+	resp, err := tclient.PostForm(localhostURL(path), *values)
+	defer resp.Body.Close()
+	if err != nil {
+		return "", err
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	return string(body), err
+}
+
+func localhostURL(u string) string {
+	if u == "" {
+		return tserver.URL
+	} else if u[0] == '/' {
+		return tserver.URL + u
+	} else {
+		return tserver.URL + "/" + u
+	}
+}
+
+func tclientGet(rqURL string) (*http.Response, error) {
+	return tclient.Get(localhostURL(rqURL))
+}
+
+func tclientPostForm(rqURL string) (*http.Response, error) {
+	return tclient.PostForm(localhostURL(rqURL), url.Values{})
 }
