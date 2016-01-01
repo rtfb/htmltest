1beff41c127a1c1ca3c54a9bc4b4713cc4f641b2
Vytautas Šaltenis <vytas@rtfb.lt>
2016-01-01 20:43:09 +0200

    Copy over higher level CSS selector helpers

--- htmltest.go
+++ htmltest.go
@@ -9,6 +9,7 @@ import (
 	"testing"
 
 	"github.com/rtfb/go-html-transform/css/selector"
+	"github.com/rtfb/go-html-transform/h5"
 	"golang.org/x/net/html"
 )
 
@@ -46,6 +47,48 @@ func CssSelect(t *testing.T, node *html.Node, query string) []*html.Node {
 		t.Fatalf("Error: query=%q, err=%s", query, err.Error())
 	}
 	return chain.Find(node)
+}
+
+func Query(t *testing.T, url, method, query string) []*html.Node {
+	switch method {
+	case "+":
+		return queryPlus(t, url, query)
+	case "*":
+		return queryStar(t, url, query)
+	case "1":
+		return []*html.Node{QueryOne(t, url, query)}
+	default:
+		t.Fatalf("Error: unknown query method: %q", method)
+	}
+	return nil
+}
+
+// QueryOne ensures there's only one thing that matches.
+func QueryOne(t *testing.T, url, q string) *html.Node {
+	nodes := queryPlus(t, url, q)
+	if len(nodes) > 1 {
+		t.Fatalf("Too many matches (%d) for node: %q", len(nodes), q)
+	}
+	return nodes[0]
+}
+
+// queryPlus is like + in regular expressions: requires one or more matches.
+func queryPlus(t *testing.T, url, query string) []*html.Node {
+	nodes := queryStar(t, url, query)
+	if len(nodes) == 0 {
+		t.Fatalf("No nodes found: %q", query)
+	}
+	return nodes
+}
+
+// queryStar is like * in regular expressions: requires zero or more matches.
+func queryStar(t *testing.T, url, query string) []*html.Node {
+	html := Curl(url)
+	doctree, err := h5.NewFromString(html)
+	if err != nil {
+		t.Fatalf("h5.NewFromString(%s) = err %q", html, err.Error())
+	}
+	return CssSelect(t, doctree.Top(), query)
 }
 
 func curlParam(url string, method func(string) (*http.Response, error)) string {
